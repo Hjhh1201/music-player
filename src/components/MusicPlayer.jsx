28462efe201a7f00881a9744d683c92bb6a6f369
scyjh4 <scyjh4@exmail.nottingham.ac.uk>
2026-01-19 23:51:17 +0800

    Initial commit

---
+++ src/components/MusicPlayer.jsx
@@ -0,0 +1,117 @@
+import { useEffect, useRef } from "react";
+import { useMusic } from "../hooks/useMusic"
+
+export const MusicPlayer = () => {
+    const {currentTrack, currentTime, setCurrentTime, formatTime,
+         duration, setDuration, nextTrack, prevTrack,
+          isPlaying, play, pause, volume, setVolume} = useMusic();
+    const audioRef = useRef(null);
+    
+    useEffect(() => {
+        const audio = audioRef.current;
+        if (!audio) return;
+        
+        if(isPlaying){
+            audio.play().catch((err) => console.error(err));
+        }else{
+            audio.pause();
+        }
+    }, [isPlaying]);
+
+    useEffect(() => {
+        const audio = audioRef.current;
+        if (!audio) return;
+
+        audio.volume=volume;
+    }, [volume]);
+
+    useEffect(() => {
+        const audio = audioRef.current;
+        if (!audio) return;
+
+        const handleLoadedMetada = () => {
+            setDuration(audio.duration)
+        };
+
+        const handleTimeUpdate = () => {
+            setCurrentTime(audio.currentTime);
+        };
+
+        const handleEnded = () => {
+            nextTrack();
+        };
+
+        audio.addEventListener("loadedmetadata", handleLoadedMetada);
+        audio.addEventListener("canplay", handleLoadedMetada);
+        audio.addEventListener("timeupdate", handleTimeUpdate);
+        audio.addEventListener("ended", handleEnded);
+
+        return () => {
+            audio.removeEventListener("loadedmetadata", handleLoadedMetada);
+            audio.removeEventListener("canplay", handleLoadedMetada);
+            audio.removeEventListener("timeupdate", handleTimeUpdate);
+            audio.removeEventListener("ended", handleEnded);
+        };
+    }, [setDuration, setCurrentTime, currentTrack, nextTrack]);
+
+    const handleVolumeChange = (e) => {
+        setVolume(parseFloat(e.target.value));
+    }
+
+    const handleTimeChange = (e) => {
+        const audio = audioRef.current;
+        if (!audio) return;
+        const newTime = parseFloat(e.target.value);
+        audio.currentTime = newTime;
+        setCurrentTime(newTime);
+    };
+
+    useEffect(() => {
+        const audio = audioRef.current;
+        if (!audio) return;
+        audio.load();
+        setCurrentTime(0);
+        setDuration(0);
+
+    }, [currentTrack, setCurrentTime, setDuration]);
+
+    const progressPercentage = duration>0 ? (currentTime/duration)*100 : 100;
+
+    return (
+        <div className="music-player">
+            <audio ref={audioRef} src={currentTrack.url} preload="metadata" crossOrigin="anonymous"/>
+
+            <div className="track-info">
+                <h3 className="track-title">{currentTrack.title}</h3>
+                <p className="track-artist">{currentTrack.artist}</p>
+            </div>
+
+            <div className="progress-container">
+                <span className="time">{formatTime(currentTime)}</span>
+                <input type="range" min="0" max={duration || 0} 
+                step="0.1" value={currentTime || 0} className="progress-bar" onChange={handleTimeChange}/>
+                <span className="time">{formatTime(duration)}</span>
+            </div>
+
+            <div className="controls">
+                <button className="control-btn" onClick={prevTrack}>
+                    ⏮
+                </button>
+                <button className="control-btn play-btn"
+                 onClick={ () => (isPlaying ? pause() : play())} >
+                {isPlaying ? "⏸" : "▶"}
+                </button>
+                <button className="control-btn" onClick={nextTrack}>
+                    ⏭
+                </button>
+            </div>
+
+            <div className="volume-container">
+                <span className="volume-icon"></span>
+                <input type="range" min="0" max="1" step="0.1" className="volume-bar"
+                 onChange={handleVolumeChange} value={volume} style={{ "--progress": `${progressPercentage}%` }}/>
+
+            </div>
+        </div>
+    );
+}
